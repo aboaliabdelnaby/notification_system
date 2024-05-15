1dc4d46262e1b7fb3054d365936b4a586ec865bc
aboali-abdelnaby <a.AbdulelNaby@likecard.com>
2024-05-15 15:14:45 +0300

    task

---
+++ app/Listeners/SendPasswordResetEmail.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Jobs\SendPasswordChangedEmailJob;
+use App\Jobs\SendWelcomeEmailJob;
+use App\Models\User;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class SendPasswordResetEmail
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(PasswordReset $event): void
+    {
+        //
+        $user = $event->user;
+        dispatch(new SendPasswordChangedEmailJob($user->toarray()));
+
+    }
+}
